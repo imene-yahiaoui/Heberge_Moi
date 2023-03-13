ba2c44098d7b49700dcb6070abffdd005167492a
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-03-13 16:00:44 +0100

    add Private Routes

--- frontend/src/components/RoutesPath/RoutesPath.jsx
+++ frontend/src/components/RoutesPath/RoutesPath.jsx
@@ -5,16 +5,22 @@ import About from "../about/About";
 import NotFound from "../notFound/NotFound";
 import Add from "../AddAccommodate/Add";
 import Login from "../Login/Login";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../helpers/features/userSlice"
+import ProtectrdRoute from "../../helpers/protectrdRoute";
+import Protect from "../../helpers/protect";
+
 
 const RoutesPath = ({ posts, dataAbout }) => {
+  const user = useSelector(selectUser);
   return (
     <div>
       <Routes>
         <Route path="/" exact element={<Home posts={posts} />} />
         <Route path="/Housing/:id" element={<Housing posts={posts} />} />
         <Route path="/About" element={<About dataAbout={dataAbout} />} />
-        <Route path="/Add" element={<Add />} />
-        <Route path="/Login" element={<Login />} />
+        <Route path="/Add"  element={  <ProtectrdRoute  user={user} > <Add /></ProtectrdRoute>} />
+        <Route path="/Login" element={ <Protect  user={user}> <Login /></Protect>} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </div>
