3ae6f7d7107ba351de8ac9461d473aedce049952
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-03-14 22:50:00 +0100

    Delete image from backend when deleting Accommodate

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,4 +1,5 @@
 const Accommodate = require("../models/Accommodate");
+const fs = require('fs');
 
 exports.createAccommodate = (req, res, next) => {
   delete req.body._id;
@@ -85,33 +86,29 @@ exports.modifyAccommodate = (req, res, next) => {
 };
 
 exports.deleteAccommodate = (req, res, next) => {
-  Accommodate.deleteOne({ _id: req.params.id })
-    .then(() => {
-      res.status(200).json({
-        message: "Deleted!",
-      });
+  Accommodate.findOne({ _id: req.params.id })
+    .then((accommodate) => {
+      
+        const filename = accommodate.cover.split("/images/")[1];
+        fs.unlink(`images/${filename}`, () => {
+          Accommodate.deleteOne({ _id: req.params.id })
+            .then(() => {
+              res.status(200).json({
+                message: "Deleted!",
+              });
+            })
+            .catch((error) => {
+              res.status(400).json({
+                error: error,
+              });
+            });
+        });
+       
     })
     .catch((error) => {
-      res.status(400).json({
-        error: error,
-      });
+      res.status(500).json({ error });
     });
 };
-
-// exports.deleteAccommodate = (req, res, next) => {
-//   Accommodate.deleteMany({ _id: req.params.id })
-//     .then(() => {
-//       res.status(200).json({
-//         message: "Deleted!",
-//       });
-//     })
-//     .catch((error) => {
-//       res.status(400).json({
-//         error: error,
-//       });
-//     });
-// };
-//await Character.deleteMany({ name: /Stark/, age: { $gte: 18 } });
 
 exports.getAllStuff = (req, res, next) => {
   Accommodate.find()
